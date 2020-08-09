2d37a81b1e5e9dd0d9ff6d8bf27fb6fa1d0ddb97
Nik1am <61971270+Nik1am@users.noreply.github.com>
2020-08-09 15:44:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,9 @@
          var ctx = drawingCanvas.getContext('2d');
 
             ctrl.oninput = function() {
+               let cbg = document.getElementById("cbg")
+               let deb = document.getElementById("debug")
+               let cbgcol = document.getElementById("bgcolor")
                let col = document.getElementById("textcolor")
                let size = document.getElementById("fsize")
                let font = `${size.value}`+`px `+`${tfont.value}`
@@ -17,11 +20,17 @@
 
             ctx.canvas.width  = canx.value;
             ctx.canvas.height = cany.value;
+            if(cbg.checked){
+               ctx.fillStyle = cbgcol.value;
+               ctx.rect(0,00,drawingCanvas.width,drawingCanvas.height);
+               ctx.fill()
+            }
 
             ctx.textBaseline = "hanging";
             ctx.fillStyle = col.value;
             ctx.font = font;
             ctx.fillText(text.value,x.value,y.value);
+            
             };
          };
      };
